43a7facfac3593ea0be5603620255325c4b302b0
Milana Kagan <75909901+milanaKagan@users.noreply.github.com>
2021-08-31 22:39:50 +0300

    Update anon dao functions

---
+++ anon-dao.js
@@ -0,0 +1,112 @@
+const raw_repo = require('./raw_repo')
+function try_func(f) {
+        try {
+                f();
+        }
+        catch (e) {
+                return e.message
+        }
+}
+function get_flight_by_id(id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_flight_by_id(${id})`);
+                //console.log(result.rows[0].sp_get_flight_by_id);
+                 return result.rows[0].sp_get_flight_by_id;
+        }
+        return try_func(f);
+}
+function get_flights_by_airline_id(id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_flights_by_airline_id(${id})`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_flights_by_parameters(origin_country_id, destination_country_id, date) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_flights_by_parameters(${origin_country_id},
+                        ${destination_country_id},'${date}')`);
+               //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_arrival_flights(country_id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_arrival_flights(${country_id})`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_departure_flights(country_id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_departure_flights(${country_id})`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_all_flights() {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_all_flights()`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_country_by_id(id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_country_by_id(${id})`);
+                //console.log(result.rows[0].sp_get_country_by_id);
+                return result.rows[0].sp_get_country_by_id;
+        }
+        return try_func(f);
+}
+function get_all_countries() {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_all_countries()`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_all_airlines() {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_all_airlines()`);
+                //console.log(result.rows);
+                return result.rows;
+        }
+        return try_func(f);
+}
+function get_airline_by_id(id) {
+
+        const f = async () => {
+                const result = await raw_repo.getRawResult(`select sp_get_airline_by_id(${id})`);
+                //console.log(result.rows[0].sp_get_airline_by_id);
+                return result.rows[0].sp_get_airline_by_id;
+        }
+        return try_func(f);
+}
+module.exports = {
+        get_flight_by_id,
+        get_all_flights,
+        get_all_airlines,
+        get_airline_by_id,
+        get_all_countries,
+        get_country_by_id,
+        get_arrival_flights,
+        get_departure_flights,
+        get_flights_by_airline_id,
+        get_flights_by_parameters
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const anon_dao = require('./anon-dao');
+//anon_dao.get_flight_by_id(1);
+//anon_dao.get_all_flights();
+//anon_dao.get_all_airlines();
+//anon_dao.get_airline_by_id(10);
+//anon_dao.get_all_countries();
+//anon_dao.get_country_by_id(20);
+//anon_dao.get_arrival_flights(6);
+//anon_dao.get_departure_flights(5);
+//anon_dao.get_flights_by_airline_id(4);
+//anon_dao.get_flights_by_parameters(5,6,'2021-08-31 23:00:00')
