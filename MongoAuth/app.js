e566cad3eef9a15854e865ee4355af89ee7ac545
Milana Kagan <75909901+milanaKagan@users.noreply.github.com>
2021-09-10 12:37:25 +0300

    mongo auth update

--- MongoAuth/app.js
+++ MongoAuth/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth,checkUser } = require('./middleware/authMiddleware');
 
 
 const app = express();
@@ -26,23 +27,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth,(req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
-// routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
-app.use(authRoutes);
-// cookies
-app.get('/set-cookies', (req, res) => {
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-  res.send('you got the cookies!');
-});
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-  res.json(cookies);
-});
